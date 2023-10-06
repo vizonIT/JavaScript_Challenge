9ec66e259f69772d98cc2b74049b9703e05697bd
vizonIT <osukakvick@gmail.com>
2023-10-07 00:38:39 +0100

    Code adjustments

--- Gynastics.js
+++ Gynastics.js
@@ -2,11 +2,11 @@
 
 // Average Score for Dolphins with Data1:
 // Data 1: score 96, 108 and 89.
-const averageDolphin1 = (96 + 108 + 89) / 3;
+const averageDolphin1 = Math.floor((96 + 108 + 89) / 3);
 // Average Score for Koalas with Data1:
 // Data 1: score 88, 91 and 110
-const averageKoalas1 = (88 + 91 + 89) / 3;
-console.log(averageDolphin1, averageKoalas1);
+const averageKoalas1 = Math.floor((88 + 91 + 89) / 3);
+console.log(`Case1`); console.log(`Dolphin: ${averageDolphin1};  Koalas: ${averageKoalas1}.`);
 
 if (averageDolphin1 > averageKoalas1) {
   console.log(`Team Dolphin wins the competition. Congratulations! 🍾🎊`)
@@ -17,11 +17,11 @@ if (averageDolphin1 > averageKoalas1) {
 
 // Average Score for Dolphins with Bonus Data1:
 // Data Bonus 1: scores 97, 112 and 101.
-const averageDolphin2 = (97 + 112 + 101) / 3;
+const averageDolphin2 = Math.floor((97 + 112 + 101) / 3);
 // Average Score for Koalas with Bonus Data1:
 // Data Bonus 1: scores 109, 95 and 123.
-const averageKoalas2 = (109 + 95 + 123) / 3;
-console.log(averageDolphin2, averageKoalas2);
+const averageKoalas2 = Math.floor((109 + 95 + 123) / 3);
+console.log(`Case2`); console.log(`Dolphin: ${averageDolphin2}, Koalas: ${averageKoalas2}.`);
 
 // Computing the Winner!
 if (averageDolphin2 > averageKoalas2 && averageDolphin2 >= 100) {
@@ -39,7 +39,7 @@ const averageDolphin3 = Math.floor((97 + 112 + 101) / 3);
 // Average Score for Koalas with Bonus Data2:
 // Data Bonus 2: scores 109, 95 and 106.
 const averageKoalas3 = Math.floor((109 + 95 + 106) / 3);
-console.log(averageDolphin3, averageKoalas3);
+console.log(`Case3`); console.log(`Dolphin: ${averageDolphin3}, Koalas: ${averageKoalas3}.`);
 
 // Computing the Winner!
 if (averageDolphin3 > averageKoalas3 && averageDolphin3 >= 100) {
